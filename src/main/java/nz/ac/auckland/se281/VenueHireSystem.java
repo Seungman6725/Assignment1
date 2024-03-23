072fea45750e420c1f7d6112252cba77daf6c588
Andrew <ajeo563@aucklanduni.ac.nz>
2024-03-23 15:39:30 +1300

    Added the differing conditions depending on the number of venues in the system and logic to printing the according statement

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -1,12 +1,8 @@
 package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
-
 import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
-
-
-
 
 public class VenueHireSystem {
 
@@ -14,87 +10,123 @@ public class VenueHireSystem {
 
   int numberOfVenues = 0;
 
-  
   public void printVenues() {
-    
-    switch(numberOfVenues){
 
+    switch (numberOfVenues) {
       case 0:
         System.out.println("There are no venues in the system. Please create a venue first.");
         break;
-      
+
       case 1:
         System.out.println("There is one venue in the system:");
-        MessageCli.VENUE_ENTRY.printMessage()
+        MessageCli.VENUE_ENTRY.printMessage(
+            venueList.get(0).get_Venue_Name(),
+            venueList.get(0).get_Venue_Code(),
+            venueList.get(0).get_Venue_Capacity(),
+            venueList.get(0).get_Venue_Hire_Fee(),
+            "TODO");
+        break;
+    }
 
+    // When the number of venues is > 1 but < 10
+    if (numberOfVenues > 1 && numberOfVenues < 10) {
+
+      MessageCli.NUMBER_VENUES.printMessage("are", Integer.toString(numberOfVenues));
+
+      for (WeddingVenue venue : venueList) {
+
+        MessageCli.VENUE_ENTRY.printMessage(
+            venue.get_Venue_Name(),
+            venue.get_Venue_Code(),
+            venue.get_Venue_Capacity(),
+            venue.get_Venue_Hire_Fee(),
+            "TODO");
+      }
+    }
+
+    // When the number of venues is 10 or more
+    else if (numberOfVenues >= 10) {
+
+      MessageCli.NUMBER_VENUES.printMessage("are", Integer.toString(numberOfVenues));
+
+      for (WeddingVenue venue : venueList) {
+
+        MessageCli.VENUE_ENTRY.printMessage(
+            venue.get_Venue_Name(),
+            venue.get_Venue_Code(),
+            venue.get_Venue_Capacity(),
+            venue.get_Venue_Hire_Fee(),
+            "TODO");
+      }
     }
   }
 
-  public void createVenue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
+  public void createVenue(
+      String venueName, String venueCode, String capacityInput, String hireFeeInput) {
 
-    //If the venue name is empty
-    if(venueName.isEmpty()){
+    // If the venue name is empty
+    if (venueName.isEmpty()) {
 
       System.out.println("Venue not created: venue name must not be empty.");
 
       return;
     }
-    
-    //If there are two instances of the same venue code in the system
-    for (WeddingVenue venue: venueList){
 
-      if ((venue.get_Venue_Code()).equals(venueCode)){
+    // If there are two instances of the same venue code in the system
+    for (WeddingVenue venue : venueList) {
 
-        MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venue.get_Venue_Code(),venue.get_Venue_Name());
-        
+      if ((venue.get_Venue_Code()).equals(venueCode)) {
+
+        MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(
+            venue.get_Venue_Code(), venue.get_Venue_Name());
+
         return;
       }
-
     }
 
-    //If the capacity isnt a whole integer, isnt negative, isnt zero
+    // If the capacity isnt a whole integer, isnt negative, isnt zero
     try {
-      
+
       int int_venue_capacity = Integer.parseInt(capacityInput);
 
-      if(int_venue_capacity <= 0){
+      if (int_venue_capacity <= 0) {
 
         System.out.println("Venue not created: capacity must be a positive number.");
 
         return;
       }
 
-    }catch (Exception e){
-      
+    } catch (Exception e) {
+
       System.out.println("Venue not created: capacity must be a number.");
 
       return;
     }
 
-    //If the hire fee isnt a whole integer, isnt negative, isnt zero
+    // If the hire fee isnt a whole integer, isnt negative, isnt zero
     try {
-      
+
       int int_venue_hire_fee = Integer.parseInt(hireFeeInput);
 
-      if(int_venue_hire_fee <= 0){
+      if (int_venue_hire_fee <= 0) {
 
         System.out.println("Venue not created: hire fee must be a positive number.");
 
         return;
       }
 
-    }catch (Exception e){
-      
+    } catch (Exception e) {
+
       System.out.println("Venue not created: hire fee must be a number.");
 
       return;
     }
 
-    //If none of these conditions are met add to arraylist
-    venueList.add(new WeddingVenue(venueName,venueCode,capacityInput,hireFeeInput));
+    // If none of these conditions are met, add to arraylist
+    venueList.add(new WeddingVenue(venueName, venueCode, capacityInput, hireFeeInput));
 
-    MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName,venueCode);
-    
+    MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
+
     numberOfVenues++;
   }
 
