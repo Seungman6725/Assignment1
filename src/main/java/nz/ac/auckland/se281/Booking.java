d478b310cbe1559bcb981fd0189820e1b660156c
Andrew <ajeo563@aucklanduni.ac.nz>
2024-03-30 18:17:45 +1300

    Finished making the Booking class with added constructor and get methods

--- src/main/java/nz/ac/auckland/se281/Booking.java
+++ src/main/java/nz/ac/auckland/se281/Booking.java
@@ -6,4 +6,32 @@ public class Booking {
   private String bookingReference;
   private String venueName;
   private String numberOfAtendees;
+
+  public Booking(String date, String bookingReference, String venueName, String numberOfAtendees) {
+
+    this.date = date;
+    this.bookingReference = bookingReference;
+    this.venueName = venueName;
+    this.numberOfAtendees = numberOfAtendees;
+  }
+
+  public String getBookingDate() {
+
+    return date;
+  }
+
+  public String getBookingReference() {
+
+    return bookingReference;
+  }
+
+  public String getVenueName() {
+
+    return venueName;
+  }
+
+  public String getNumberOfAtendees() {
+
+    return numberOfAtendees;
+  }
 }
